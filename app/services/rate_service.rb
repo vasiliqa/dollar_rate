5973dad279054c6e5c9dec9747dbe41561c341c6
Vasilisa Tyulberova <vasiliqa@rambler.ru>
2017-02-24 22:50:53 +0700

    Add update_rate method with specs

--- app/services/rate_service.rb
+++ app/services/rate_service.rb
@@ -6,4 +6,27 @@ class RateService
 
     frate_is_valid ? frate : Rate.first
   end
+
+  def update_rate
+    Rate.first_or_initialize.update(value: get_rate_from_api)
+  end
+
+  private
+
+  def get_rate_from_api
+    uri = URI('http://apilayer.net/api/live')
+    params = { access_key: ENV['CURRENCY_LAYER_KEY'], currencies: 'RUB' }
+    uri.query = URI.encode_www_form(params)
+
+    response = Net::HTTP.get_response(uri)
+
+    raise RateAPIError, 'Request to Rate API is not successful' if response.code != '200'
+
+    data = ActiveSupport::JSON.decode(response.body)
+    data['quotes']['USDRUB'].round(4)
+
+  rescue RateAPIError, RateAPIError => e
+  end
 end
+
+class RateAPIError < StandardError; end
--- spec/services/rate_service_spec.rb
+++ spec/services/rate_service_spec.rb
@@ -1,60 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe RateService do
-  let(:usual_rate) { create :rate }
-  let(:valid_forced_rate) { create :forced_rate }
-  let(:expired_forced_rate) { build :forced_rate, expiry_time: Time.zone.now - 1.day }
+  describe 'check_rate method' do
+    let(:usual_rate) { create :rate }
+    let(:valid_forced_rate) { create :forced_rate }
+    let(:expired_forced_rate) { build :forced_rate, expiry_time: Time.zone.now - 1.day }
 
-  subject { described_class.new.check_rate }
+    subject { described_class.new.check_rate }
 
-  context 'when we have no rates at all' do
-    it 'returns nil' do
-      expect(subject).to eq nil
-    end
-  end
-
-  context 'when we have no forced rate' do
-    before { usual_rate }
-
-    it 'returns usual rate' do
-      expect(subject).to eq usual_rate
-    end
-  end
-
-  context 'when we have valid forced rate' do
-    before { valid_forced_rate }
-
-    context 'and usual rate' do
-      before { usual_rate }
-
-      it 'returns forced rate' do
-        expect(subject).to eq valid_forced_rate
-      end
-    end
-
-    context 'and no usual rate' do
-      it 'returns forced rate' do
-        expect(subject).to eq valid_forced_rate
-      end
-    end
-  end
-
-  context 'when we have expired forced rate' do
-    before { expired_forced_rate.save(validate: false) }
-
-    context 'and usual rate' do
-      before { usual_rate }
-
-      it 'returns usual rate' do
-        expect(subject).to eq usual_rate
-      end
-    end
-
-    context 'and no usual rate' do
+    context 'when we have no rates at all' do
       it 'returns nil' do
         expect(subject).to eq nil
       end
     end
+
+    context 'when we have no forced rate' do
+      before { usual_rate }
+
+      it 'returns the usual rate' do
+        expect(subject).to eq usual_rate
+      end
+    end
+
+    context 'when we have the valid forced rate' do
+      before { valid_forced_rate }
+
+      context 'and the usual rate' do
+        before { usual_rate }
+
+        it 'returns the forced rate' do
+          expect(subject).to eq valid_forced_rate
+        end
+      end
+
+      context 'and no usual rate' do
+        it 'returns the forced rate' do
+          expect(subject).to eq valid_forced_rate
+        end
+      end
+    end
+
+    context 'when we have the expired forced rate' do
+      before { expired_forced_rate.save(validate: false) }
+
+      context 'and the usual rate' do
+        before { usual_rate }
+
+        it 'returns the usual rate' do
+          expect(subject).to eq usual_rate
+        end
+      end
+
+      context 'and no usual rate' do
+        it 'returns nil' do
+          expect(subject).to eq nil
+        end
+      end
+    end
   end
 
+  describe 'update_rate method' do
+    let!(:rate) { create :rate, value: 50 }
+
+    subject { described_class.new.update_rate }
+
+    context 'when API is available' do
+      it 'updates rate from API', vcr: true do
+        subject
+        expect(Rate.first.value).to eq 58.5005
+      end
+    end
+
+    context 'when API is not available', vcr: true do
+      it 'does not raise error' do
+        expect { subject }.not_to raise_error
+      end
+
+      it 'does not update rate from API' do
+        subject
+        expect(Rate.first.value).to eq 50
+      end
+    end
+  end
 end
